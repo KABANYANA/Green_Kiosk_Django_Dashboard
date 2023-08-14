a6d46422c57aa3e38affa5bf1e6f68ef00fd68c4
KABANYANA <joyeusekabanyana@gmail.com>
2023-08-14 09:15:30 +0300

    Added views to handle functions and templates to create user interface

--- cart/models.py
+++ cart/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from inventory.models import Product
-from orders.models import Orders
+# from orders.models import Orders
 # Create your models here.
 class Cart(models.Model):
-    Orders= models.ForeignKey(Orders,on_delete=models.PROTECT)
+    # Orders= models.ForeignKey(Orders,on_delete=models.PROTECT)
     Product=models.ManyToManyField(Product)
     number = models.PositiveIntegerField
     size = models.PositiveIntegerField()
--- customers/models.py
+++ customers/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
-from orders.models import Orders
+# from django.contrib.auth.models import User
+# from orders.models import Orders
 
     
 # Create your models here.
 class Customers(models.Model):
-    Orders= models.ForeignKey(Orders,on_delete=models.PROTECT)
-    User= models.OneToOneField(User,on_delete=models.CASCADE)
+    # Orders= models.ForeignKey(Orders,on_delete=models.PROTECT)
+    # User= models.OneToOneField(User,on_delete=models.CASCADE)
     name = models.CharField(max_length=32)
     email = models.EmailField()
     phone_Number = models.CharField(max_length=15)
